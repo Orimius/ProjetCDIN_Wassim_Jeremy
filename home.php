fe657ed8543b68a905967cfb6359b6b05bafcf27
Orimius <55831728+Orimius@users.noreply.github.com>
2019-11-27 08:57:31 +0100

    Update home.php

--- home.php
+++ home.php
@@ -8,8 +8,8 @@ if(isset($_POST["raz"])) {
 		}
 	}
 }
-if(isset($_POST["player"])) {
-	$id = $_POST["player"];
+if(isset($_POST["joueur"])) {
+	$id = $_POST["joueur"];
 	$_SESSION["POS_".$PagePos] = $id;
 }
 
@@ -49,3 +49,13 @@ $posList = array(
 	<title>Gestion équipe</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span>Menu</span>
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </nav>
+</body>
+</html>

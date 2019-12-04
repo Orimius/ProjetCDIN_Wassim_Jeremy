9b56705b7360d708ce8af02181bb6e4d2d8da4eb
Orimius <55831728+Orimius@users.noreply.github.com>
2019-12-04 08:20:48 +0100

    C3

--- accueil.php
+++ accueil.php
@@ -46,8 +46,9 @@ $posList = array(
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Gestion équipe</title>
+	<title>Gestion de l'équipe</title>
 	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="accueil.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
